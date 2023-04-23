fb537d88b4965399a3d1b1ff54527ea05b0ece55
Амир <mixenix@yandex.ru>
2023-04-23 14:59:05 +0500

    убрал комментированный код, доделал (финальная версия)

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import io
-
 import telebot
 from telebot import types
 from editors import edits
-import pydub
 
 with open('insert_token.txt') as f:
     token = f.read()
@@ -19,13 +16,11 @@ typesofedit = {'grayscale': edits.grayscale,
                'sharpen': edits.filter,
                'smooth': edits.filter,
                'Крутые фильтры': edits.pilgram_filters}
-edtypes = ('BLUR', 'CONTOUR', 'DETAIL', 'EDGE', 'EMBOSS', 'SHARPEN', 'SMOOTH')
 pilfilters = (
     '_1977', 'aden', 'brannan', 'brooklyn', 'clarendon', 'earlybird', 'gingham', 'hudson', 'inkwell', 'kelvin', 'lark',
     'lofi', 'maven', 'mayfair', 'moon', 'nashville', 'perpetua', 'reyes', 'rise', 'slumber', 'stinson', 'toaster',
     'valencia', 'walden', 'willow', 'xpro2')
 jobtype = ('Редактор картинок', 'Редактор аудио')
-audiofx = ('Повышение тона', 'Понижение тона')
 
 def generate_buttons(bts_names, markup):
     for button in bts_names:
@@ -50,7 +45,7 @@ def yes_no_checker(message, img):
         bot.register_next_step_handler(message, yes_no_checker, img)
 
 
-def pilg_filters(message, edittype, img=None):
+def pilg_filters(message, img=None):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup = generate_buttons(pilfilters, markup)
     bot.send_message(message.chat.id, "Выбери тип фильтра", reply_markup=markup)
@@ -67,66 +62,12 @@ def start_pic(message):
     bot.send_message(message.chat.id, "Привет, выбери тип фильтра для своего изображения", reply_markup=markup)
     bot.register_next_step_handler(message, next_step_photo1)
 
-
-# @bot.message_handler(commands=['audio'])
-# def start_audio(message):
-#     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-#     markup = generate_buttons(audiofx, markup)
-#     bot.send_message(message.chat.id, "Выбери эффект для своего аудио", reply_markup=markup)
-#     bot.register_next_step_handler(message, next_step_audio1)
-
-# def voice_processing(message):
-#     file_info = bot.get_file(message.voice.file_id)
-#     downloaded_file = bot.download_file(file_info.file_path)
-#     with open('new_file.ogg', 'wb') as new_file:
-#         new_file.write(downloaded_file)
-#     return (downloaded_file, new_file)
-
-
-# def next_step_audio1(message):
-#     if message.content_type == 'text':
-#         audiofxtype = message.text.lower()
-#         if audiofxtype == 'повышение тона':
-#             bot.send_message(message.chat.id, 'Отправьте аудио, которому хотите повысить тон')
-#             bot.register_next_step_handler(message, pitch_up)
-#         elif audiofxtype == 'понижение тона':
-#             bot.send_message(message.chat.id, 'Отправьте аудио, которому хотите понизить тон')
-#             bot.register_next_step_handler(message, pitch_down)
-#         else:
-#             bot.send_message(message.chat.id, "Выбери эффект.")
-#             bot.register_next_step_handler(message, next_step_audio1)
-#     else:
-#         bot.send_message(message.chat.id, "Выбери эффект.")
-#         bot.register_next_step_handler(message, next_step_audio1)
-# def pitch_up(message):
-#     if message.content_type == 'voice':
-#         octaves = 0.5
-#         audioTuple = voice_processing(message)
-#         audioBytes = audioTuple[0]
-#         audio = audioTuple[1]
-#         print(audio, type(audio))
-#         sound = pydub.AudioSegment.from_ogg(audio, format='wav')
-#         new_sample_rate = int(sound.frame_rate * (2.0 ** octaves))
-#         hipitch_sound = sound._spawn(sound.raw_data, overrides={'frame_rate': new_sample_rate})
-#         hipitch_sound = hipitch_sound.set_frame_rate(44100)
-#         bot.send_voice(message.chat.id, hipitch_sound)
-#         print(type(audio))
-#     else:
-#         bot.send_message(message.chat.id, "Отправьте аудио голосовым сообщением")
-#         bot.register_next_step_handler(message, pitch_up)
-
-# def pitch_down(message):
-#     if message.content_type == 'voice':
-#         audio = voice_processing(message)
-#     else:
-#         bot.send_message(message.chat.id, "Отправьте аудио голосовым сообщением")
-#         bot.register_next_step_handler(message, pitch_down)
 def next_step_photo1(message, img=None):
     if img is None:
         if message.content_type == 'text':
             edittype = message.text.lower()
             if edittype == 'крутые фильтры':
-                pilg_filters(message, edittype)
+                pilg_filters(message)
             elif edittype == 'reset':
                 bot.clear_step_handler(message)
             elif edittype in pilfilters:
@@ -150,7 +91,7 @@ def next_step_photo1(message, img=None):
         if message.content_type == 'text':
             edittype = message.text.lower()
             if edittype == 'крутые фильтры':
-                pilg_filters(message, edittype, img=img)
+                pilg_filters(message, img=img)
             elif edittype in pilfilters:
                 next_step_photo2(message, edittype, img=img)
             else:
